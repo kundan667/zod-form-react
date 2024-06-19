84b8df74f8054bc38e57b2ee5304f102c79784a6
KUNDAN KUMAR <kundankumar@KUNDANs-MacBook-Pro.local>
2024-06-19 11:58:24 +0530

    style changes

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -4,7 +4,7 @@ function Modal() {
         <dialog id="my_modal" className="modal">
             <div className="modal-box grid content-center justify-items-center">
                 <img src="/assets/checklist.png" alt="check" width={120} />
-                <p className="py-4">Your account has been created successfully</p>
+                <p className="py-4 text-center">Your account has been created successfully</p>
                 <div className="modal-action">
                     <form method="dialog">
                         {/* if there is a button in form, it will close the modal */}
